a63a8a11b6cea022baf128b863ffd61598441019
Chukwu Gideon <chuksgid@gmail.com>
2024-02-14 18:48:33 +0100

    modified files

--- rexpay/src/main/java/com/octacore/rexpay/data/remote/models/AuthorizeCardResponse.kt
+++ rexpay/src/main/java/com/octacore/rexpay/data/remote/models/AuthorizeCardResponse.kt
@@ -1,18 +1,17 @@
-package com.octacore.pgplibs;
+@file:JvmSynthetic
+
+package com.octacore.rexpay.data.remote.models
 
 /***************************************************************************************************
  *                          Copyright (C) 2024,  Octacore Tech.
  ***************************************************************************************************
  * Project         : rexpay
  * Author          : Gideon Chukwu
- * Date            : 07/02/2024
+ * Date            : 14/02/2024
  **************************************************************************************************/
-public enum SignatureCheckResult {
-    SignatureVerified,
-    SignatureBroken,
-    PublicKeyNotMatching,
-    NoSignatureFound;
-
-    private SignatureCheckResult() {
-    }
-}
+internal data class AuthorizeCardResponse(
+    internal val paymentId: String? = null,
+    internal val transactionRef: String? = null,
+    internal val amount: String? = null,
+    internal val responseCode: String? = null
+)
